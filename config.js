3c34a732bcdc824bad1798f3bb8891c2e213532e
Noah Eilken <neilken@gmcs.org>
2026-09-17 08:08:54 -0600

    Connect student submissions to Google Sheets

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Filled after the separate Google Apps Script logger is deployed.
-window.MAP_BUILDER_ENDPOINT = "";
+window.MAP_BUILDER_ENDPOINT = "https://script.google.com/macros/s/AKfycbzMx8Q8VyzMitO-oefPpkuSBMiZsJP67heWRFVQmCv8l1bi-P5G_BgythLqUrWnoDNf/exec";
